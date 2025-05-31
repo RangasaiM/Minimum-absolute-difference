8a1591dea11632a0cb839061533054a7beb75b45
Rangasai <rangasaimangalagiri@gmail.com>
2025-05-31 15:00:10 +0530

    java test

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -4,19 +4,7 @@ import java.util.*;
 
 public class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-        Arrays.sort(arr);
-        int minDiff = Integer.MAX_VALUE;
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = 1; i < arr.length; i++) {
-            int diff = arr[i] - arr[i - 1];
-            if (diff < minDiff) {
-                minDiff = diff;
-                result.clear();
-                result.add(Arrays.asList(arr[i - 1], arr[i]));
-            } else if (diff == minDiff) {
-                result.add(Arrays.asList(arr[i - 1], arr[i]));
-            }
-        }
-        return result;
+        // Write your code here!!
+        return Collections.emptyList(); // Erase this
     }
 }
