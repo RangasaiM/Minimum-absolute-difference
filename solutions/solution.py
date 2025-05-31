8b6017ba7f8163e401c7471da050b8f031895d03
Rangasai <rangasaimangalagiri@gmail.com>
2025-05-31 15:07:52 +0530

    py test

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,4 +1,13 @@
 class Solution(object):
     def minimumAbsDifference(self, arr):
-        # Write Your code here
-        pass
+        arr.sort()
+        min_diff = float('inf')
+        result = []
+        for i in range(1, len(arr)):
+            diff = arr[i] - arr[i - 1]
+            if diff < min_diff:
+                min_diff = diff
+                result = [[arr[i - 1], arr[i]]]
+            elif diff == min_diff:
+                result.append([arr[i - 1], arr[i]])
+        return result
